bcc3cd1be998e9c0b6e3ce134fe4f184fbfdeadf
Aaron <adub65@gmail.com>
2020-05-13 14:07:48 -0400

    many relationships

--- app/course.rb
+++ app/course.rb
@@ -2,8 +2,36 @@
 
 class Course
   attr_accessor :subject
-
+  @@all = []
   def initialize(subject)
     @subject = subject
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_subject(subject)
+    all.find do |course|
+    course.subject == subject
+    end
+  end
+
+  def enrollments
+    Enrollment.all.select do |enrollment|
+      enrollment.course == self
+    end
+  end
+
+  def class_list
+    enrollments.map do |enrollment|
+      enrollment.student.full_name
+    end
+  end
+
+  def all_subjects
+    all.map(&subject).uniq
+  end
+
 end
