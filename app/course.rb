849196653ff1d480a819ec25b7186538be57a2a0
Pat McClernan <patmccler@gmail.com>
2020-05-13 11:05:52 -0400

    implements student-enrollment-supplies-courses

--- app/course.rb
+++ app/course.rb
@@ -2,8 +2,34 @@
 
 class Course
   attr_accessor :subject
+  @@all = []
 
   def initialize(subject)
     @subject = subject
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_subject(subject)
+    @@all.find { |course| course.subject == subject }
+  end
+
+  def self.all_subjects
+    all.map(&:subject).uniq
+  end
+
+  def enrollments
+    Enrollment.all.select do |enrollment|
+      enrollment.course == self
+    end
+  end
+
+  def class_list
+    enrollments.map do |enrollment|
+      enrollment.student.full_name
+    end
   end
 end
