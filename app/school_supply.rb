d22277214fbe56299ca4e2d526460a4b8e8a601b
Jo Fischer <josephinefischer9@gmail.com>
2020-05-13 10:33:31 -0400

    done

--- app/school_supply.rb
+++ app/school_supply.rb
@@ -3,8 +3,21 @@
 class SchoolSupply
   attr_accessor :student, :kind
 
+  @@all = []
+
   def initialize(student, kind)
     @kind = kind
     @student = student
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_all_by_kind(kind)
+    all.select do |item|
+      item.kind == kind
+    end
   end
 end
